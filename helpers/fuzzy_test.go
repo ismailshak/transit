9af68835922bececb9b702594773f9dd1f873d86
ismailshak <ismailshak94@yahoo.com>
2023-01-31 08:25:14 -0500

    Add a test

---
+++ helpers/fuzzy_test.go
@@ -0,0 +1,48 @@
+package helpers_test
+
+import (
+	"testing"
+
+	"github.com/ismailshak/transit/helpers"
+	"github.com/sahilm/fuzzy"
+	"github.com/stretchr/testify/assert"
+)
+
+func convertMatchesToSlice(matches fuzzy.Matches) []string {
+	var found []string
+	for _, m := range matches {
+		found = append(found, m.Str)
+	}
+
+	return found
+}
+
+func TestFuzzyFind(t *testing.T) {
+	testData := []string{
+		"name",
+		"long name",
+		"random word",
+		"wEiRd CaSiNg",
+		"ALL CAPS",
+		"__random*()=++characters^&name",
+	}
+
+	testCases := []struct {
+		input    string
+		expected []string
+	}{
+		{"name", []string{"name", "long name", "__random*()=++characters^&name"}},
+		{"word", []string{"random word"}},
+		{"weird CASING", []string{"wEiRd CaSiNg"}},
+		{"all caps", []string{"ALL CAPS"}},
+	}
+
+	for _, test := range testCases {
+		matches := helpers.FuzzyFind(test.input, testData)
+		assert.Len(t, test.expected, matches.Len())
+
+		result := convertMatchesToSlice(matches)
+
+		assert.ElementsMatch(t, result, test.expected)
+	}
+}
