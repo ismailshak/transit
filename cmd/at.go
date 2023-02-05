15555f5ff2a84a4db093ff03d4ef5e6ec57b22b8
ismailshak <ismailshak94@yahoo.com>
2023-02-05 13:31:49 -0500

    feat(at): rename command `list` to `at`

--- cmd/at.go
+++ cmd/at.go
@@ -1,17 +1,46 @@
-package list
+package cmd
 
 import (
 	"fmt"
 
 	"github.com/ismailshak/transit/api"
+	"github.com/ismailshak/transit/config"
 	"github.com/ismailshak/transit/helpers"
 	"github.com/ismailshak/transit/logger"
 	"github.com/ismailshak/transit/tui"
 	"github.com/sahilm/fuzzy"
+	"github.com/spf13/cobra"
 )
 
-// Entry point to the `list` subcommand
-func Execute(client api.Api, args []string) {
+var atCmd = &cobra.Command{
+	Use:     "at <args>",
+	Example: "  transit at courth (matches \"Court House\")\n  transit at metro (matches \"Metro Center\")",
+	Short:   "Display upcoming train arrival information at chosen station(s)",
+	Long: `
+Display upcoming train information for one or more stations.
+
+Arguments are considered valid if it can be used to narrow
+the official station names to just 1. If something's too generic,
+try being more specific by adding more characters.
+	`,
+	Args: cobra.MinimumNArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		location := config.GetConfig().Core.Location
+		client := api.GetClient(location)
+		if client == nil {
+			helpers.Exit(helpers.EXIT_BAD_CONFIG)
+		}
+
+		ExecuteAt(client, args)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(atCmd)
+}
+
+// Entry point to the `at` subcommand
+func ExecuteAt(client api.Api, args []string) {
 	keys := helpers.GetDmvStationNames()
 
 	for _, arg := range args {
